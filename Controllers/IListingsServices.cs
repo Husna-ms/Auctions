c8d37036418f1c7a164c328ef206966d10c728ac
Husna-ms <husna9886@bazeuniversity.edu.ng>
2024-11-26 16:51:43 +0100

    Add project files.

---
+++ Controllers/IListingsServices.cs
@@ -0,0 +1,13 @@
+﻿using Auctions.Models;
+
+
+namespace Auctions.Data.Services
+{
+
+    public interface  IListingsServices
+    {
+        IQueryable<Listing> GetAll();
+        Task Add(Listing listing);
+        Task<Listing> GetById(int? id);
+    }
+}
